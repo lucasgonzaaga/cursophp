1eb7ff2c3554bf484f2d6af539bb623f7a855c7d
lucasgonzaaga <lucasgonzagasantoss020106@gmail.com>
2025-02-13 16:00:14 -0300

    subindo as coisas do curso

---
+++ ex001/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex 01</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+        echo "slaoqla sla oq";
+        ?>
+    </h1>
+    <p>
+        sla oq la sla oq la
+    </p>
+</body>
+
+</html>
---
+++ ex003/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>exercicio 3</h1>
+
+    <?php
+    date_default_timezone_set("America/Sao_Paulo");
+    echo "slaoqslaoqla " . date("d/m/y");
+    echo " sla oq sla oq la " . date("g:i:s");
+    ?>
+</body>
+
+</html>
