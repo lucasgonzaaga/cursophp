afea9c9447aef6900ce8d83ac165af4198d9771e
lucasgonzaaga <lucasgonzagasantoss020106@gmail.com>
2025-02-13 17:01:07 -0300

    terminar a calculadora em casa

---
+++ testes/testes.php
@@ -0,0 +1,19 @@
+<script>
+    // alert('fazendo uma calculadora bizarra sem lembrar como se programa, so bora')
+    let primeiroNumero = prompt('fala o primeiro numero ai')
+    let segundoNumero = prompt('fala o segundo numero ai')
+    let operador = prompt('fala o operador ai')
+    let resultado = Number()
+    if (operador == '+') {
+        resultado == Number(primeiroNumero) + Number(segundoNumero)
+    } else if (operador = '-') {
+        resultado == Number(primeiroNumero) - Number(segundoNumero)
+    } else if (operador = '*') {
+        resultado == Number(primeiroNumero) * Number(segundoNumero)
+    } else if (operador = '/') {
+        resultado == Number(primeiroNumero) / Number(segundoNumero)
+    } else {
+        alert(operador)
+    }
+    alert('o resultado é: ' + resultado)
+</script>
