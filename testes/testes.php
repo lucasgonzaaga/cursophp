3532f4422246e9ebb4cd8ef77daba38da0651be5
lucasgonzaaga <lucasgonzagasantoss020106@gmail.com>
2025-02-14 17:12:29 -0300

    fix bug teste.php

--- testes/testes.php
+++ testes/testes.php
@@ -3,15 +3,15 @@
     let primeiroNumero = prompt('fala o primeiro numero ai')
     let segundoNumero = prompt('fala o segundo numero ai')
     let operador = prompt('fala o operador ai')
-    let resultado = Number()
-    if (operador == '+') {
-        resultado == Number(primeiroNumero) + Number(segundoNumero)
-    } else if (operador = '-') {
-        resultado == Number(primeiroNumero) - Number(segundoNumero)
-    } else if (operador = '*') {
-        resultado == Number(primeiroNumero) * Number(segundoNumero)
-    } else if (operador = '/') {
-        resultado == Number(primeiroNumero) / Number(segundoNumero)
+    let resultado
+    if (operador === '+') {
+        resultado = Number(primeiroNumero) + Number(segundoNumero)
+    } else if (operador === '-') {
+        resultado = Number(primeiroNumero) - Number(segundoNumero)
+    } else if (operador === '*') {
+        resultado = Number(primeiroNumero) * Number(segundoNumero)
+    } else if (operador === '/') {
+        resultado = Number(primeiroNumero) / Number(segundoNumero)
     } else {
         alert(operador)
     }
